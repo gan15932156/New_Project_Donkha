317f3e78be82cee95888f18a4914d19b78b84d31
gan <powerganoflove@hotmail.com>
2019-09-23 22:24:35 +0700

    tu

---
+++ app/src/main/java/com/example/logintest/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.example.logintest;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fregment_home, container, false);
+        Toast.makeText(getContext(), "Homeeeee", Toast.LENGTH_SHORT).show();
+        return view;
+    }
+}
--- app/src/main/java/com/example/logintest/MainUser.java
+++ app/src/main/java/com/example/logintest/MainUser.java
@@ -1,23 +1,28 @@
 package com.example.logintest;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainUser extends AppCompatActivity {
-    private TextView txtname;
     private Button btnlogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
 
-        txtname = findViewById(R.id.txtName);
         btnlogout = findViewById(R.id.btnUserLogout);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
         btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +34,25 @@ public class MainUser extends AppCompatActivity {
                 finish();
             }
         });
-        txtname.setText("Hello "+PreferenceUtils.getUsername(this));
-        Toast.makeText(this, "Hello"+PreferenceUtils.getUsername(this), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello "+PreferenceUtils.getUsername(this), Toast.LENGTH_SHORT).show();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null ;
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_statement:
+                    selectedFragment = new StatementFragment();
+                    break;
+                case R.id.nav_me:
+                    selectedFragment = new MeFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,selectedFragment).commit();
+            return true;
+        }
+    };
 }
